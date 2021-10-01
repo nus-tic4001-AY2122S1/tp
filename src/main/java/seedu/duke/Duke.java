c0c8cb5fa85db8b284be33d1b1df350596c145de
yongming <zhuangyongming1900@gmail.com>
2021-10-01 21:21:06 +0800

    Init commit, add project backbone.

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,21 +1,29 @@
 package seedu.duke;
 
+import ui.Ui;
+
 import java.util.Scanner;
 
 public class Duke {
+    private final Ui ui;
+
+    public Duke() {
+        this.ui = new Ui();
+    }
+
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
+        new Duke().run();
+    }
 
+    public void run() {
+        this.ui.welcome();
         Scanner in = new Scanner(System.in);
         System.out.println("Hello " + in.nextLine());
+
+        // exit program
+        System.exit(0);
     }
 }
---
+++ src/main/java/ui/Ui.java
@@ -0,0 +1,15 @@
+package ui;
+
+/**
+ * Ui class is only for printing message.
+ */
+public class Ui {
+
+    public void welcome () {
+        print("Hello! I'm Dafy\n" );
+    }
+
+    public void print(String message) {
+        System.out.println( message);
+    }
+}
