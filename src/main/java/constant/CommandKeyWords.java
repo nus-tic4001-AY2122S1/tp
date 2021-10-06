89eaa356747d3182ae9552aa65f7ca57ae290d04
yongming <zhuangyongming1900@gmail.com>
2021-10-07 02:01:12 +0800

    Add CommandKeyWords constant to keep track a list of predefined keywords

---
+++ src/main/java/constant/CommandKeyWords.java
@@ -0,0 +1,53 @@
+package constant;
+
+import exception.ErrorHandler;
+
+public enum CommandKeyWords {
+    LOCATION("LOCATION");
+    private String value;
+
+    /**
+     * @return string which shows all available command keywords
+     */
+    private static String getSerializedString() {
+        String keywords = "";
+        int numberOfKeys = values().length;
+        int index=0;
+        for(CommandKeyWords v : values()) {
+            index += 1;
+            String punctuation = index == numberOfKeys ? "." : ", ";
+            keywords = keywords.concat(v.getValue()).concat(punctuation);
+        }
+
+        return keywords;
+    }
+
+    CommandKeyWords(String value) {
+        this.value = value;
+    }
+
+    /**
+     * @return enum value
+     */
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return this.getValue();
+    }
+
+    /**
+     * @param value is an enum string for validating if the given value is valid
+     * @throws ErrorHandler customized error
+     */
+    public static CommandKeyWords getEnum(String value) throws ErrorHandler {
+        for(CommandKeyWords v : values())
+            if(v.getValue().equalsIgnoreCase(value)) return v;
+        throw new ErrorHandler(ErrorMessage.INVALID_COMMAND + " Please starts your command with "
+                + getSerializedString()
+                + " (case insensitive)");
+    }
+}
+
