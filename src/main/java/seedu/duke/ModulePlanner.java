097a14db24afc1dd7253b87de562e20f4a5e5938
James <jamessoh11@gmail.com>
2021-10-06 21:48:55 +0800

    Added api, module, module planner class and update build.gradle

---
+++ src/main/java/seedu/duke/ModulePlanner.java
@@ -0,0 +1,12 @@
+package seedu.duke;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ModulePlanner {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Welcome to the module planner!");
+        Api api = new Api();
+        Module[] allModules = api.getAllModules();
+    }
+}
