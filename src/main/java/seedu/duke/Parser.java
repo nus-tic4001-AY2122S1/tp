3390d76450e62a9e9564fe206e500aeba57b7cbe
IcyNoPeople <leowjunhui2@hotmail.com>
2021-10-07 22:48:37 +0800

    Parser class

---
+++ src/main/java/seedu/duke/Parser.java
@@ -0,0 +1,15 @@
+package seedu.duke;
+
+public class Parser {
+    public static String parse(String fullCommand) {
+        String keyCommand = fullCommand.split(" ")[0].toLowerCase();
+        switch(keyCommand) {
+            case ("list"):
+                return "list";
+
+            default:
+                return "lol";
+        }
+
+    }
+}
