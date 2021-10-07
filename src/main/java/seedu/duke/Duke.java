e8de2a7dff87c4b94ba52a048eb87ef7bf9329b0
James <jamessoh11@gmail.com>
2021-10-08 00:03:02 +0800

    Revert duke naming

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -3,7 +3,7 @@ package seedu.duke;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class ModulePlanner {
+public class Duke {
     public static void main(String[] args) throws IOException {
         System.out.println("Welcome to the module planner!");
         Api api = new Api();
