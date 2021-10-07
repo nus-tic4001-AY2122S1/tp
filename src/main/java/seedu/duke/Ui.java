4d06b2854bc3c4e3cef41f81eca520abf7b0e4a4
IcyNoPeople <leowjunhui2@hotmail.com>
2021-10-07 21:06:47 +0800

    added ui to read user input

---
+++ src/main/java/seedu/duke/Ui.java
@@ -0,0 +1,23 @@
+package seedu.duke;
+
+import java.util.Scanner;
+
+public class Ui {
+
+    public static void Welcome(){
+
+        System.out.println("Welcome to NUS module tracker \n" +
+                "What can I do for you?\n");
+        
+    }
+
+
+    public static String readCommand(){
+        String line;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Type something: ");
+        line = in.nextLine();
+        return line;
+    }
+
+}
