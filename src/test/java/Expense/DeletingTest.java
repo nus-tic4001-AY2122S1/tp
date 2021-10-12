362d08112ae8b6484a7bb9162caf3c405f71c1a7
YangShuogeng <54273624+YangShuogeng@users.noreply.github.com>
2021-10-12 23:25:17 +0800

    Changed import file.

--- src/test/java/Expense/DeletingTest.java
+++ src/test/java/Expense/DeletingTest.java
@@ -1,7 +1,7 @@
 package Expense;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
 import java.util.Date;
 
