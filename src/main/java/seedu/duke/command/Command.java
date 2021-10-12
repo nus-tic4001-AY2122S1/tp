d3d79651efac1ecef99fbb4c2d1b44e10636997e
WANG WENWEI <e0373972@u.nus.edu>
2021-10-12 20:25:34 +0800

    List: display all expenses

---
+++ src/main/java/seedu/duke/command/Command.java
@@ -0,0 +1,12 @@
+package seedu.duke.command;
+
+import java.util.List;
+import java.io.IOException;
+
+public interface Command {
+    List<String> run(String[] fullCommand) throws IOException;
+
+    default boolean isExit(){
+        return false;
+    }
+}
---
+++ src/main/java/seedu/duke/task/Task.java
@@ -0,0 +1,15 @@
+package seedu.duke.task;
+
+import seedu.duke.task.TaskList;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task {
+    protected String description;
+
+    public Task(String description){
+        this.description = description;
+    }
+
+
+}
