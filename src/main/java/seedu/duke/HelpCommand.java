c0078f039a28c96cec65eb823d30c75598e5483f
zcnmashleu95 <zcnmashleu@gmail.com>
2021-10-13 00:04:26 +0800

    Touchup Help

--- src/main/java/seedu/duke/HelpCommand.java
+++ src/main/java/seedu/duke/HelpCommand.java
@@ -6,7 +6,8 @@ public class HelpCommand {
     }
 
     public void execute() {
-        String instructions = DeleteCommand.getHelp() +
+        String instructions = "Commands: " + "\n" +
+                DeleteCommand.getHelp() +
                 "\n" +
                 "add [Appointment_Description] /s [Start_Time] /e [End_Time]";
         System.out.println(instructions);
