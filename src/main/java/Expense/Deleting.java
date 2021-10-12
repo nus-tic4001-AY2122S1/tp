4d65f0fb17e2730ffe0fcd323111d82c2a5a9b5f
YangShuogeng <54273624+YangShuogeng@users.noreply.github.com>
2021-10-12 23:19:54 +0800

    Added one Assert Unit test function and updated Delete class

--- src/main/java/Expense/Deleting.java
+++ src/main/java/Expense/Deleting.java
@@ -3,15 +3,15 @@ package Expense;
 import java.util.ArrayList;
 
 public class Deleting {
-    private int expenseIndex;
+    //private int expenseIndex;
     private ArrayList<Expense> expList;
 
-    Deleting(int index, ArrayList expList) {
-        this.expenseIndex = index;
+    Deleting(ArrayList expList) {
+        this.expList = expList;
     }
 
     ArrayList deleteExpense(int index){
-        this.expList.remove(expenseIndex);
+        this.expList.remove(index);
 
         return this.expList;
     }
