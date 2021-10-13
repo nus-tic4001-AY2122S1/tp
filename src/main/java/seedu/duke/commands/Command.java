e9d5df0e61727386cf1c6a4894110044a9324b3c
Ruiting1 <ruiting245831717@gmail.com>
2021-10-13 22:57:17 +0800

    setData for whole hashtable of list

--- src/main/java/seedu/duke/commands/Command.java
+++ src/main/java/seedu/duke/commands/Command.java
@@ -1,11 +1,24 @@
 package seedu.duke.commands;
 
+import seedu.duke.storage.GTDList;
+
+import java.util.Hashtable;
+
 public class Command {
 
-    public Command(){
+    protected Hashtable<String, GTDList> GTDLists;
+
+    public Command() {
 
     }
 
     public void execute() {
+    }
+
+    /**
+     * Supplies the data the command will operate on.
+     */
+    public void setData(Hashtable<String, GTDList> GTDLists) {
+        this.GTDLists = GTDLists;
     }
 }
