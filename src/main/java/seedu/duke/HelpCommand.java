42fbb6b0f6619313ce0c8dd33d6693c151c2dc4a
zcnmashleu95 <zcnmashleu@gmail.com>
2021-10-13 21:37:25 +0800

    Fix Help

--- src/main/java/seedu/duke/HelpCommand.java
+++ src/main/java/seedu/duke/HelpCommand.java
@@ -6,11 +6,11 @@ public class HelpCommand {
     }
 
     public void execute() {
-        String instructions = "Commands: " +
-                "\n" +
-                DeleteCommand.getHelp() +
-                "\n" +
-                "add [Appointment_Description] /s [Start_Time] /e [End_Time]";
+        String instructions = "Commands: "
+                + "\n"
+                + DeleteCommand.getHelp()
+                + "\n"
+                + "add [Appointment_Description] /s [Start_Time] /e [End_Time]";
         System.out.println(instructions);
     }
 }
