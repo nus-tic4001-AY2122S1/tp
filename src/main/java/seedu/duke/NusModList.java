ed01460680f9a8e059aeb029f26d23c4a3e00763
IcyNoPeople <leowjunhui2@hotmail.com>
2021-10-14 01:36:14 +0800

    j-unit

--- src/main/java/seedu/duke/NusModList.java
+++ src/main/java/seedu/duke/NusModList.java
@@ -13,7 +13,6 @@ public class NusModList {
     }
 
     public Module getMod(String moduleCode) {
-
         Module module = new Module();
         for (int i = 0; i < getSize(); i++) {
             if (ModListForYear[i].getModuleCode() == moduleCode) {
---
+++ src/test/java/seedu/duke/command/NusModListTest.java
@@ -0,0 +1,33 @@
+package seedu.duke.command;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.duke.Module;
+import seedu.duke.NusModList;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NusModListTest {
+    @Test
+    public void NusModListTest1() {
+
+        Integer[] sem = {1};
+
+        Module module1 = new Module("TIC4001", "Software Engineering Practicum I", sem, "This module is one of the two modules where students get to practice software engineering concepts learnt in a team environment to produce a well-designed, well-tested, large-scaled software system. This module focuses on software engineering practice of a greenfield project, where students will elicit the requirements, design the software architecture and interfaces, and implement the software from scratch.");
+        Module module2 = new Module("TIC4005", "Parallel and Distributed Software Engineering", sem, "This module provides a broad introduction to software engineering approaches specific to building a parallel/distributed software system. Such systems are challenging since they are concurrent, prone to components failure, and non-deterministic. This module begins by introducing students to the basics of concurrent and parallel programming, as well as common middleware and tools for building distributed systems. It then covers how behaviours and challenges specific to distributed and parallel software can be considered as part of the software engineering processes (including requirements, design, and testing).");
+        Module[] modulelist = {module1, module2};
+
+        NusModList nusModList = new NusModList(modulelist);
+
+        assertEquals(2, nusModList.getSize());
+        assertEquals(module1, nusModList.getMod("TIC4001"));
+        assertEquals(false, nusModList.hasMod("TIC4002"));
+        assertEquals(true, nusModList.hasMod("TIC4005"));
+        assertEquals(0, nusModList.moduleIndex("TIC4001"));
+        assertEquals(module1, nusModList.getMod(0));
+
+    }
+}
