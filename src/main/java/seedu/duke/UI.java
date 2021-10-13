18aff4b3bbc56ca1bfc6793c9ba0a8f140564166
zyjarvis <zyjarvis@gmail.com>
2021-10-13 22:23:23 +0800

    v1.0 update

--- src/main/java/seedu/duke/UI.java
+++ src/main/java/seedu/duke/UI.java
@@ -3,9 +3,6 @@ package seedu.duke;
 import java.util.Scanner;
 
 public class UI {
-    /**
-     * Welcome message to be shown when the programme starts.
-     */
     public static void welcome() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
