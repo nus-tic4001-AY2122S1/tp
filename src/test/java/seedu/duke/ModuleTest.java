333b2237392039d2f8ed8b89861c7caca6ec171e
James <jamessoh11@gmail.com>
2021-10-14 19:09:05 +0800

    style

--- src/test/java/seedu/duke/ModuleTest.java
+++ src/test/java/seedu/duke/ModuleTest.java
@@ -4,14 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ModuleTest {
     @Test
     public void testModule() {
         System.out.println("Module constructor should work with args, and getters return correct data");
-        Semesterdata semData[] = new Semesterdata[1];
-        Integer semesters[] = {1,2};
+        Semesterdata[] semData = new Semesterdata[1];
+        Integer[] semesters = {1,2};
         semData[0] = new Semesterdata(1, new Date(), 100);
         Module mod = new Module("TIC4001", "Software Eng", semesters, "A description",
                 4.0F, "Computing", "Faculty of Comp", "TIC2001",
