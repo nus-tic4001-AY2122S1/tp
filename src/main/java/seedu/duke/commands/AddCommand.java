03e49b2b92ab6454d321c9542a7a766f499bfda3
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2021-10-14 22:33:30 +0800

    Add line separator to AddCommand

--- src/main/java/seedu/duke/commands/AddCommand.java
+++ src/main/java/seedu/duke/commands/AddCommand.java
@@ -16,7 +16,7 @@ public class AddCommand extends Command {
 
     @Override
     public void execute() {
-        System.out.println("This is an add command, the added task is " + taskname);
+        System.out.println("This is an add command, the added task is:" + System.lineSeparator() + taskname);
         GTDLists.get("inbox").add(new GTDThought(taskname));
     }
 
