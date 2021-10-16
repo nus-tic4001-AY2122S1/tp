e088317f358a12f5083d2cb8adff41a772b6e932
yongming <zhuangyongming1900@gmail.com>
2021-10-16 17:52:19 +0800

    Update location command class and parser class to handle SET_LOCATION command.

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -5,6 +5,7 @@ import command.Command;
 import command.DeleteCommand;
 import command.TodoCommand;
 import command.ViewCommand;
+import command.LocationCommand;
 import constant.CommandKeyWords;
 import constant.ErrorMessage;
 import exception.ErrorHandler;
@@ -18,24 +19,38 @@ public class Parser {
         CommandKeyWords commandWord = CommandKeyWords.getEnum(result[0].toUpperCase());
 
         switch (commandWord) {
-        case VIEW:
-            return new ViewCommand();
-        case TODO:
-            if (result.length < 2) {
-                throw new ErrorHandler(ErrorMessage.EMPTY_TODO);
-            }
-            this.content = result[1].trim();
-            return new TodoCommand(this.content);
-        case DELETE:
-            if (result.length < 2) {
-                throw new ErrorHandler(ErrorMessage.EMPTY_TASK_NUMBER);
-            }
-            this.taskNo = result[1].trim();
+            case VIEW:
+                return new ViewCommand();
+            case TODO:
+                if (result.length < 2) {
+                    throw new ErrorHandler(ErrorMessage.EMPTY_TODO);
+                }
+                this.content = result[1].trim();
+                return new TodoCommand(this.content);
+            case DELETE:
+                if (result.length < 2) {
+                    throw new ErrorHandler(ErrorMessage.EMPTY_TASK_NUMBER);
+                }
+                this.taskNo = result[1].trim();
 
-            return new DeleteCommand(this.taskNo);
-        case BYE:
-        default:
-            return new ByeCommand();
+                return new DeleteCommand(this.taskNo);
+            case SET_LOCATION:
+                if (result.length < 2) {
+                    throw new ErrorHandler(ErrorMessage.EMPTY_TASK_NUMBER);
+                }
+                String[] inputContent = result[1].split(" ", 2);
+
+                if (inputContent.length < 2) {
+                    throw new ErrorHandler(ErrorMessage.EMPTY_LOCATION_DESCRIPTION);
+                }
+
+                this.taskNo = inputContent[0];
+                this.content = inputContent[1];
+
+                return new LocationCommand(this.taskNo, this.content);
+            case BYE:
+            default:
+                return new ByeCommand();
         }
     }
 }
