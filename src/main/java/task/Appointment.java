90a5a67c2c0722ab51d4b0b4d1774bfba501c6f9
yongming <zhuangyongming1900@gmail.com>
2021-10-17 22:24:40 +0800

    Update appointment class to include location and time

--- src/main/java/task/Appointment.java
+++ src/main/java/task/Appointment.java
@@ -2,15 +2,20 @@ package task;
 
 public class Appointment extends Task {
     private final String type;
+    private final String at;
     private String location;
 
-    public Appointment(String description) {
+    public Appointment(String description, String at, String location) {
         super(description);
         type = "A";
+        this.location = location;
+        this.at = at;
     }
 
     public String taskToStringFormat() {
-        return "[" + this.getType() + "]" + "[" + super.getStatusIcon() + "] " + super.getDescription();
+        return "[" + this.getType() + "]" + "[" + super.getStatusIcon() + "] "
+                + super.getDescription() + "," + " at " + this.at + ". "
+                + "Location: " + this.location;
     }
 
     public String getType() {
