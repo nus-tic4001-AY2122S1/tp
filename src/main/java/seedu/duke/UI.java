77af6064cdd43a9e7d16f034be781e8d279debad
WANG WENWEI <e0373972@u.nus.edu>
2021-10-19 22:32:06 +0800

    update code

--- src/main/java/seedu/duke/UI.java
+++ src/main/java/seedu/duke/UI.java
@@ -7,12 +7,12 @@ import java.util.Scanner;
 
 public class UI {
     public static void welcome() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //String logo = " ____        _        \n"
+                //+ "|  _ \\ _   _| | _____ \n"
+                //+ "| | | | | | | |/ / _ \\\n"
+                //+ "| |_| | |_| |   <  __/\n"
+                //+ "|____/ \\__,_|_|\\_\\___|\n";
+        //System.out.println("Hello from\n" + logo);
         System.out.println("What can I help you?");
     }
 
