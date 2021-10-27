894203fc0bb6ef5311eb3e3676bc5143676b8f60
WANG WENWEI <e0373972@u.nus.edu>
2021-10-27 22:25:37 +0800

    new ListCommandTest

--- src/test/java/seedu/duke/command/ListCommandTest.java
+++ src/test/java/seedu/duke/command/ListCommandTest.java
@@ -1,46 +1,35 @@
-/*
 package seedu.duke.command;
 
 import org.junit.jupiter.api.Test;
 import seedu.duke.ItemList;
-import seedu.duke.InputErrorException;
 import seedu.duke.item.Item;
 
 import java.sql.Timestamp;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ListCommandTest {
+public class ListCommandTest {
 
     @Test
-    public void getCommandTest() throws ParseException, InputErrorException {
-
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
-
-        String d1s = "11-Dec-2021";
-        String d2s = "12-Dec-2021";
-        Date d1 = formatter.parse(d1s);
-        Date d2 = formatter.parse(d2s);
+    public void getCommandTest() {
 
         Date recordDate = new Date();
         long time = recordDate.getTime();
         Timestamp timestamp = new Timestamp(time);
 
-        Item testerExpense1 = new Item(d1, "taxi", 22, timestamp);
-        Item testerExpense2 = new Item(d2, "dinner", 33, timestamp);
+        Item testerIncome = new Item("salary", "monthly income", 30, timestamp);
+        Item testerExpense = new Item("lunch", "F&B", 33, timestamp);
 
         ArrayList<Item> arrayListExpLst = new ArrayList<>();
         ArrayList<Item> expectedArrayLst = new ArrayList<>();
 
-        arrayListExpLst.add(testerExpense1);
-        arrayListExpLst.add(testerExpense2);
+        arrayListExpLst.add(testerIncome);
+        arrayListExpLst.add(testerExpense);
 
-        expectedArrayLst.add(testerExpense1);
-        expectedArrayLst.add(testerExpense2);
+        expectedArrayLst.add(testerIncome);
+        expectedArrayLst.add(testerExpense);
 
         ItemList newExpLst = new ItemList(arrayListExpLst);
         ItemList expect = new ItemList(expectedArrayLst);
@@ -49,8 +38,6 @@ class ListCommandTest {
 
         listCommand.run(newExpLst);
 
-        assertEquals(expect.expenses, newExpLst.expenses);
+        assertEquals(expect.items, newExpLst.items);
     }
 }
-
- */
