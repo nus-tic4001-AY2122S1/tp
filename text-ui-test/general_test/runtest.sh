00b1b45f01b1ccffab29e6c79f747261d11490e4
zcnmashleu95 <zcnmashleu@gmail.com>
2021-10-28 18:10:50 +0800

    Fixing runtest.sh path bug

--- text-ui-test/general_test/runtest.sh
+++ text-ui-test/general_test/runtest.sh
@@ -10,9 +10,9 @@ cd text-ui-test
 
 java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < general_test/input.txt > general_test/ACTUAL.TXT
 
-cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix EXPECTED-UNIX.TXT ACTUAL.TXT
-diff EXPECTED-UNIX.TXT ACTUAL.TXT
+cp ../../text-ui-test/general_test/EXPECTED.TXT ../../text-ui-test/general_test/EXPECTED-UNIX.TXT
+dos2unix ../../text-ui-test/general_test/EXPECTED-UNIX.TXT ../../text-ui-test/general_test/ACTUAL.TXT
+diff ../../text-ui-test/general_test/EXPECTED-UNIX.TXT ../../text-ui-test/general_test/ACTUAL.TXT
 if [ $? -eq 0 ]
 then
     echo "Test passed!"
