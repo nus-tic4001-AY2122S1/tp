9abadf1914453d9010eb27ba154b7a824fc81ad5
zcnmashleu95 <zcnmashleu@gmail.com>
2021-10-28 18:14:07 +0800

    New Fix to runtest.sh

--- text-ui-test/general_test/runtest.sh
+++ text-ui-test/general_test/runtest.sh
@@ -10,9 +10,9 @@ cd text-ui-test
 
 java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < general_test/input.txt > general_test/ACTUAL.TXT
 
-cp ../../text-ui-test/general_test/EXPECTED.TXT ../../text-ui-test/general_test/EXPECTED-UNIX.TXT
-dos2unix ../../text-ui-test/general_test/EXPECTED-UNIX.TXT ../../text-ui-test/general_test/ACTUAL.TXT
-diff ../../text-ui-test/general_test/EXPECTED-UNIX.TXT ../../text-ui-test/general_test/ACTUAL.TXT
+cp general_test/EXPECTED.TXT general_test/EXPECTED-UNIX.TXT
+dos2unix text-ui-test/general_test/EXPECTED-UNIX.TXT general_test/ACTUAL.TXT
+diff general_test/EXPECTED-UNIX.TXT general_test/ACTUAL.TXT
 if [ $? -eq 0 ]
 then
     echo "Test passed!"
