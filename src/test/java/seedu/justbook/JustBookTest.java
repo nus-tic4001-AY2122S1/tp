dae6493d3ba9f3895bdf7db1deb61113062ec4d6
Gwee Yeu Chai <gweeyc@live.com.sg>
2021-11-02 13:55:47 +0800

    Created Better JUnit tests in JustBookTest.java.

--- src/test/java/seedu/justbook/JustBookTest.java
+++ src/test/java/seedu/justbook/JustBookTest.java
@@ -4,55 +4,52 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.List;
-
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDate;
+import java.util.HashMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DisplayName("JUnit 5 Testing")
+@DisplayName("JUnit 5 JustBook Testing")
 public class JustBookTest {
-    private static List<Bookings> bookings;
+    static HashMap<LocalDate, LocalDate> POLICE = JustBook.BLOCKLIST;
+    private static final PrintStream stdOut = System.out;
+    private static final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
     @BeforeAll
-    static void setup() {
-        bookings = JustBook.appointments;
-
-        try {
-            JustBook.onLoad();
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex.getMessage());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    static void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+        POLICE.put(LocalDate.of(2021,6,8), LocalDate.of(2021, 6, 30));
     }
 
     @Test
-    @DisplayName("`del a` (formula code test)")
-    public void delAllFormulaTest() {
-        bookings.clear();
-        assertEquals(0, bookings.size());
+    @DisplayName("`block` CLI Command output test")
+    public void setBlockRulesTest() {
+        JustBook.setBlockRules("block", "2021-6-6 2021-6-5");
+
+        assertEquals("Start date cannot be equal to or after the end date. Try again?",
+                outputStreamCaptor.toString()
+                .trim());
+        outputStreamCaptor.reset();
     }
 
     @Test
-    @DisplayName("`undel a` (formula code test)")
-    public void unDelAllFormulaTest() {
-        bookings.clear();
+    @DisplayName("`unblock` CLI Command test")
+    public void setBlockRulesTest_2() {
+        JustBook.setBlockRules("unblock", "2021-6-8 2021-6-30");
 
-        try {
-            JustBook.onLoad();
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex.getMessage());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        assertEquals(18, bookings.size());
+        assertEquals("Date Range: \"08/06/2021 - 30/06/2021\" has been successfully unblocked out in your scheduler",
+                outputStreamCaptor.toString()
+                        .trim());
+        outputStreamCaptor.reset();
+        // tests successful removal of block entry in POLICE
+        assertEquals(0,POLICE.size());
     }
 
     @AfterAll
     static void tearDown() {
-        bookings = null;
+        System.setOut(stdOut);
+        POLICE.clear();
+        POLICE = null;
     }
 }
