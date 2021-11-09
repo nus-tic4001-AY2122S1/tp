7ea47a7ce291b78b1423009e2aa3b6276b56e25c
YangShuogeng <54273624+YangShuogeng@users.noreply.github.com>
2021-11-10 01:34:30 +0800

    Update InputErrorException.java

--- src/main/java/seedu/duke/InputErrorException.java
+++ src/main/java/seedu/duke/InputErrorException.java
@@ -8,7 +8,7 @@ public class InputErrorException extends Exception {
     }
 
     /**
-     * The function to return error type
+     * The function to return error type.
      * @return error type
      */
     public String getErrorType() {
@@ -16,21 +16,21 @@ public class InputErrorException extends Exception {
     }
 
     /**
-     * The function to print input data format error message to user
+     * The function to print input data format error message to user.
      */
     static void toPrintDateFormatError() {
         System.out.println("OOPS!!! The input date format wrong. Please try again.\n");
     }
 
     /**
-     * The function to print input list format error message to user
+     * The function to print input list format error message to user.
      */
     static void toPrintListFormatError() {
         System.out.println("OOPS!!! The list input format wrong. Please try again.\n");
     }
 
     /**
-     * The function to print input sort format error to user
+     * The function to print input sort format error to user.
      */
     static void toPrintSortFormatError() {
         System.out.println("OOPS!!! The sort input format wrong. Please try again.\n");
@@ -39,14 +39,14 @@ public class InputErrorException extends Exception {
     }
 
     /**
-     * The function to print empty file error message to user
+     * The function to print empty file error message to user.
      */
     static void toPrintEmptyFileError() {
         System.out.println("OOPS!!! The list is empty. Please read the file or add expenses.\n");
     }
 
     /**
-     * The function to print invalid command error message to user
+     * The function to print invalid command error message to user.
      */
     static void toPrintInvalidCommand() {
 
@@ -54,14 +54,14 @@ public class InputErrorException extends Exception {
     }
 
     /**
-     * The function to print index not integer error message to user
+     * The function to print index not integer error message to user.
      */
     static void toPrintIndexNotInt() {
         System.out.println("OOPS!!! The index you typed is not an integer. Please try again.\n");
     }
 
     /**
-     * The function to print index out of range error message to user
+     * The function to print index out of range error message to user.
      */
     static void toPrintIndexOutOfRange() {
 
@@ -69,14 +69,14 @@ public class InputErrorException extends Exception {
     }
 
     /**
-     * The function to print amount not a number error message to user
+     * The function to print amount not a number error message to user.
      */
     static void toPrintAmountNotNumber() {
         System.out.println("OOPS!!! The amount is not a number. Please try again.\n");
     }
 
     /**
-     * The function to print input information not enough error message to user
+     * The function to print input information not enough error message to user.
      */
     static void toPrintInputInfoShortage() {
         System.out.println("OOPS!!! The input information not enough. Please try again.\n");
