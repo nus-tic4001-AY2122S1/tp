6f685c31d5509419afb1ae466fd1ca0e55c242b9
yinyin377 <54065563+yinyin924@users.noreply.github.com>
2021-11-10 08:24:35 +0800

    Update JUnit for Save command test

---
+++ src/test/java/seedu/justbook/SaveCommandTest.java
@@ -0,0 +1,21 @@
+package seedu.justbook;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SaveCommandTest {
+    @Test
+    public void getSaveTest() {
+        File file = new File("data/justbook.txt");
+        boolean expectedResult = true;
+        boolean actualResult = false;
+        if (file.exists()){
+            actualResult = true;
+        }
+        assertEquals(expectedResult, actualResult);
+    }
+}
